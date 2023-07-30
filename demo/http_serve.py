b80170d2ffbdb50c746093dceb7fa0a00b7d8eb6
c-fandango <edd568@hotmail.co.uk>
2023-07-30 13:14:47 +0100

    init demo scripts

---
+++ demo/http_serve.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+
+HOSTNAME = "localhost"
+PORT = 5555
+DATA_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_FILE = os.path.join(DATA_DIR, "http_response.json")
+
+
+class ServeRooms(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/api/v1/rooms.get":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(self.load_responseb(DATA_FILE))
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+    def load_responseb(self, file_path):
+        with open(file_path, "r") as fle:
+            data = fle.read()
+
+        return data.encode()
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((HOSTNAME, PORT), ServeRooms)
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
---
+++ demo/ws_serve.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import asyncio
+import pathlib
+import websockets
+import json
+import time
+import os
+
+HOSTNAME = "localhost"
+WSS_PORT = 5550
+DATA_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_FILE = os.path.join(DATA_DIR, "ws_responses.json")
+
+
+class WSResponse:
+    def __init__(self, data_path):
+        self.playbook = self.load_playbook(data_path)
+        print("loaded data")
+
+    def load_playbook(self, file_path):
+        with open(file_path, "r") as fle:
+            data = json.load(fle)
+
+        return data
+
+    async def serve(self, websocket):
+        for response in self.playbook["init"]:
+            data = await websocket.recv()
+
+            message = json.loads(data)
+
+            content = message.get("msg")
+
+            if content == response["trigger_msg"]:
+                send_msg = response["response"]
+
+            send_msg["id"] = message.get("id")
+
+            print(f"sending {send_msg}")
+            await websocket.send(json.dumps(send_msg))
+
+        for response in self.playbook["room_msgs"]:
+            time.sleep(response["timing"])
+            send_msg = response["response"]
+
+            await websocket.send(json.dumps(send_msg))
+
+
+async def main():
+    ws_response = WSResponse(DATA_FILE)
+
+    async with websockets.serve(ws_response.serve, "127.0.0.1", WSS_PORT):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
